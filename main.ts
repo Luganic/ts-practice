d8186210bf0a8b99ac800b53eb501f1332eb4311
이관현\Luganic <rhksgus509@gmail.com>
2025-06-18 19:12:16 +0900

    객체 타입 연습해보기

--- main.ts
+++ main.ts
@@ -3,3 +3,6 @@ let itemProce: number = 129000;
 let membersOnly: boolean = true;
 let owner: undefined = undefined;
 let seller: null = null;
+
+let num = 2 / 0; // Infinity
+let num2 = 0 / 0; //NaN
