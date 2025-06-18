0aaf7fe32d91a01d34acf67bccec6ccc0eca2234
이관현\Luganic <rhksgus509@gmail.com>
2025-06-18 15:16:54 +0900

    타입스크립트 기본형

--- main.ts
+++ main.ts
@@ -1,2 +1,5 @@
-let size: number = 100;
-size = 105;
+let itemName: string = "코드잇 블랙 후드";
+let itemProce: number = 129000;
+let membersOnly: boolean = true;
+let owner: undefined = undefined;
+let seller: null = null;
