ffc32842ebd49c507013123487bff8f4cb8421f6
이관현\Luganic <rhksgus509@gmail.com>
2025-06-19 18:11:28 +0900

    interface practice

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
 "use strict";
-let size = 100;
-size = 105;
+var Size;
+(function (Size) {
+    Size["S"] = "S";
+    Size["M"] = "M";
+    Size["L"] = "L";
+    Size["XL"] = "XL";
+})(Size || (Size = {}));
+const product1 = {
+    id: "c001",
+    name: "코드잇 블랙 후드 집업",
+    price: 129000,
+    membersOnly: true,
+    sizes: [Size.M, Size.L],
+};
+const product2 = {
+    id: "d001",
+    name: "코드잇 텀블러",
+    price: 25000,
+};
+const printProduct = (product) => {
+    console.log(`${product.name}의 가격은 ${product.price}원입니다.`);
+};
+printProduct(product1);
+printProduct(product2);
--- main.ts
+++ main.ts
@@ -1,35 +1,42 @@
-// 객체 타입 선언
-let product: {
+enum Size {
+  S = "S",
+  M = "M",
+  L = "L",
+  XL = "XL",
+}
+
+interface Product {
   id: string;
   name: string;
   price: number;
   membersOnly?: boolean;
-  sizes: string[];
-} = {
-  id: "coo1",
-  name: "코드잇 블랙 후디",
-  price: 129000,
-  sizes: ["M", "L", "XL"],
-};
-
-// 동적 key 정의: 계산된 프로퍼티 이름
-if (product.membersOnly) {
-  console.log("회원 전용 상품");
-} else {
-  console.log("일반 상품");
 }
 
-// 인덱스 시그니처
-let field = "field name";
-let obj = {
-  [field]: "field value",
+interface ClothingProduct extends Product {
+  sizes: Size[];
+}
+
+const product1: ClothingProduct = {
+  id: "c001",
+  name: "코드잇 블랙 후드 집업",
+  price: 129000,
+  membersOnly: true,
+  sizes: [Size.M, Size.L],
 };
 
-let stock: {
-  [id: string]: number;
-} = {
-  coo1: 3,
-  c002: 0,
-  c003: 1,
-  c004: "codeit", // 타입 오류
+const product2: Product = {
+  id: "d001",
+  name: "코드잇 텀블러",
+  price: 25000,
 };
+
+interface PrintProductFunction {
+  (product: Product): void;
+}
+
+const printProduct: PrintProductFunction = (product) => {
+  console.log(`${product.name}의 가격은 ${product.price}원입니다.`);
+};
+
+printProduct(product1);
+printProduct(product2);
